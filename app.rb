94fdf6daadff1843db431c9ba902e5442c35b656
Clem Freeman <clemfree@gmail.com>
2015-02-04 14:39:53 -0800

    Add ablility to add name to index.erb

--- app.rb
+++ app.rb
@@ -1,9 +1,15 @@
 require 'bundler/setup'
-Bundler.require(:default, :test)
+Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*rb'].each { |file| require file }
 
 get '/' do
   @contacts = Contact.all
   erb :index
+end
+
+post '/add_contact' do
+  name = params.fetch("name")
+  Contact.new(name: name).save
+  redirect '/'
 end
